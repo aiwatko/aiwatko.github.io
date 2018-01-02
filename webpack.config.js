30cc551e63a263e15ed5dd300e0bced4fb9971ce
Oktawia Kata <oktawia.kata@siroop.ch>
2018-01-02 09:54:40 +0100

    Set up basic webpack configuration

---
+++ webpack.config.js
@@ -0,0 +1,65 @@
+var path = require('path');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var CleanWebpackPlugin = require('clean-webpack-plugin');
+
+var extractPlugin = new ExtractTextPlugin({
+   filename: 'main.css'
+});
+// We need to export our webpack configuration
+module.exports = {
+    // Where webpack starts analyzing the project
+    entry: './src/js/app.js',
+    // Where and which files are created
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: [
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['es2015']
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.scss$/,
+                use: extractPlugin.extract({
+                // Webpack executes loaders in the reverse order                     
+                    use: ['css-loader', 'sass-loader']
+                })
+            },
+            {
+                test: /\.html$/,
+                use: ['html-loader']
+            },
+            {
+                test: /\.(jpg|png)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]',
+                            outputPath: 'img/',
+                            publicPath: 'img/'
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    // Plugins are applied on the bundled files before they are output
+    plugins: [
+        extractPlugin,
+        new HtmlWebpackPlugin({
+            template: 'src/index.html'
+        }),
+        new CleanWebpackPlugin(['dist'])
+    ]
+};
