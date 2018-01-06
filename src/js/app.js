6d9e81c67afe0b945450427bb2f1df020c777eba
Oktawia Kata <oktawia.kata@siroop.ch>
2018-01-06 18:18:02 +0100

    Add simple circle animation

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,18 @@
 import '../css/main.scss';
+
+const classes = {
+    smallCircle: 't-home__small-circle',
+    largeCircle: 't-home__large-circle'
+}
+
+const smallCircle = document.getElementById(classes.smallCircle);
+const largeCircle = document.getElementById(classes.largeCircle);
+
+document.addEventListener('mousemove', evt => onMouseMove(evt));
+
+function onMouseMove(evt){
+ if(window.innerWidth > 768){
+    smallCircle.style.left = evt.screenY * 0.14;
+    largeCircle.style.right = evt.screenY * 0.04;
+ }
+};
